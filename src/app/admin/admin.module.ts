7b8dae9479f6dd24cb354b82e08e90d60547e6ba
english-mate <65681349+english-mate@users.noreply.github.com>
2021-05-30 02:12:25 +0530

    added admin add product

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AddProductFormComponent } from './components/add-product-form/add-product-form.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: 'products', component: ProductsComponent }]),
+    RouterModule.forChild([
+      { path: 'products', component: ProductsComponent },
+      { path: 'admin/add-product', component: AddProductFormComponent },
+    ]),
   ],
 })
 export class AdminModule {}
--- src/app/admin/components/add-product-form/add-product-form.component.ts
+++ src/app/admin/components/add-product-form/add-product-form.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/shared/models/product';
+import { CategoryService } from 'src/app/shared/services/category.service';
+import { ProductDataService } from 'src/app/shared/services/product-data.service';
 
 @Component({
   selector: 'app-add-product-form',
   templateUrl: './add-product-form.component.html',
-  styleUrls: ['./add-product-form.component.css']
+  styleUrls: ['./add-product-form.component.css'],
 })
 export class AddProductFormComponent implements OnInit {
+  categories: Array<String> = [];
+  product: Product = {
+    $key: '',
+    title: '',
+    price: 0,
+    category: '',
+    imageUrl: '',
+  };
+  fileToUpload: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  imageUrl: string = '/assets/imgs/noimage.png';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private productDataService: ProductDataService
+  ) {
+    this.categories = categoryService.getAll();
   }
 
+  ngOnInit(): void {}
+
+  addProduct(product: any) {
+    this.product.$key = (
+      this.productDataService.getAll().length + 1
+    ).toString();
+    this.product.title = product.name;
+    this.product.price = product.price;
+    this.product.category = product.category;
+    this.product.imageUrl = this.imageUrl;
+    console.log(this.product);
+    this.productDataService.addProduct(this.product);
+    this.router.navigate(['products']);
+  }
+
+  handleFileInput(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if (fileList) {
+      this.fileToUpload = fileList.item(0);
+    }
+    var reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    };
+    reader.readAsDataURL(this.fileToUpload);
+  }
 }
