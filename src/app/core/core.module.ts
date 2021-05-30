66d159a0de872d8ac262eb3a9b97103c1705179f
Isuru <isurumaduranga.16@cse.mrt.ac.lk>
2021-05-30 21:10:37 +0530

    Add login component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
